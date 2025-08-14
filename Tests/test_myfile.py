e87ee457f306fa60d8c17ea6e3bbebb4689662cf
Eli <elibasson2@gmail.com>
2025-08-14 14:35:46 +0300

    add file

---
+++ Tests/test_myfile.py
@@ -0,0 +1,16 @@
+def test_first_scenario():
+    a = 5
+    b = 6
+    assert a == b
+
+
+def test_second_scenario():
+    a = 5
+    b = 6
+    assert a * b == b * a
+
+
+def test_third_scenario():
+    a = 5
+    b = 6
+    assert a - b == b - a
